1ad448f1db733719826444e26e38bebb004b42d9
hoi911 <nextix88@gmail.com>
2023-08-21 16:41:57 +0900

    BottomNavigationBar add

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -12,7 +12,7 @@ class _LandingPageState extends State<LandingPage> {
   @override
   void initState() {
     Timer(Duration(seconds: 3), () {
-      Get.to(MainPage());
+      Get.offAll(MainPage());
     });
     super.initState();
   }
@@ -20,13 +20,19 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Image.asset(
-          'assets/images/landing.jpeg',
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/landing.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          CircularProgressIndicator()
+        ],
       ),
     );
   }
--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -6,12 +6,47 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  List<BottomNavigationBarItem> bottomItems = [
+    BottomNavigationBarItem(
+      label: '1번',
+      icon: Icon(Icons.favorite),
+    ),
+    BottomNavigationBarItem(
+      label: '2번',
+      icon: Icon(Icons.abc),
+    ),
+    BottomNavigationBarItem(
+      label: '3번',
+      icon: Icon(Icons.abc),
+    ),
+    BottomNavigationBarItem(
+      label: '4번',
+      icon: Icon(Icons.abc),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('메인 페이지'),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 14,
+        unselectedFontSize: 10,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: bottomItems,
+      ),
+      body: Container(),
     );
   }
 }
